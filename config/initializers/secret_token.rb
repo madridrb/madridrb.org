d3d4915c097591f0d47be4744d9d91aa580dc976
Gabriel Ortuño <arctarusnet@gmail.com>
2014-02-10 17:28:25 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Madridrb::Application.config.secret_key_base = 'e877c5a173bf2470942ce920c066cc440d9ac225abb72fb2d6747b01ce979b626d65f4175d5cce913fd3634a687dea3af6f2f8307f6f4a4984067fdca0914e46'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Madridrb::Application.config.session_store :cookie_store, key: '_madridrb_session'
