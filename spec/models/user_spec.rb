13675f37e97391704b28d166cd43211fb394eaa7
Gabriel Ortuño <arctarusnet@gmail.com>
2014-03-09 11:40:01 +0100

    user spec for generate confirmation token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe User do
+  describe 'Request reset password' do
+    context "Confirmation token" do
+      subject(:user) { create :user }
+      it { expect(user.confirmation_token).to be_nil }
+
+      it 'generate' do
+        user.forgot_password!
+        expect(user.confirmation_token).not_to be_nil
+      end
+    end
+  end
 end
