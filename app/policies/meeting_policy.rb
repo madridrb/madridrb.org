f1744d3951602a64f540f4998101a40bf726161f
Gabriel Ortuño <arctarusnet@gmail.com>
2014-06-30 13:48:51 +0200

    Only can be attend / unattend future meetings

--- app/policies/meeting_policy.rb
+++ app/policies/meeting_policy.rb
@@ -1,11 +1,11 @@
 class MeetingPolicy < ApplicationPolicy
 
   def attend?
-    user.present? && !record.attendees.include?(user)
+    user.present? && record.starts_at.future? && !record.attendees.include?(user)
   end
 
   def unattend?
-    user.present? && record.attendees.include?(user)
+    user.present? && record.starts_at.future? && record.attendees.include?(user)
   end
 
   class Scope < Struct.new(:user, :scope)
