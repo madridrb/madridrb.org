a4813c13bd8f73176b19e89559929001a33af0d6
kikito <kikito@gmail.com>
2014-09-11 18:54:01 +0200

    Remove created_at method
    (It seems it is already a time interval, no need to parse it)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,12 +9,7 @@ class Comment
 
   validates_presence_of :user_id, :user_name, :body
 
-  def created_at
-    Time.parse(read_attribute(:created_at))
-  end
-
   def user
     @user ||= User.find(user_id)
   end
-
 end
