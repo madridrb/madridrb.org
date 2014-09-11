d481415731174fce51b95a82e3009d97399c9e42
kikito <kikito@gmail.com>
2014-09-11 19:02:05 +0200

    Add should_see_selector helpers

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -12,6 +12,14 @@ module Helpers
     expect(page).to have_no_content text
   end
 
+  def should_see_selector(selector)
+    expect(page).to have_selector selector
+  end
+
+  def should_not_see_selector(selector)
+    expect(page).to have_no_selector selector
+  end
+
   def recreate!(model)
     klass = model.to_s.classify.constantize
     klass.database.delete!
