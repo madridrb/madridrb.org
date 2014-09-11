790b9a870b6eccecdc2de27aaa33e3590a8496f4
kikito <kikito@gmail.com>
2014-09-11 19:52:48 +0200

    use ids instead of strings to attempt to fix spec in travis

--- spec/features/attend_a_meeting_spec.rb
+++ spec/features/attend_a_meeting_spec.rb
@@ -22,7 +22,7 @@ feature 'Attendees' do
         should_not_see_selector "a[title='Paco Pérez']"
       end
 
-      click_on 'Attend'
+      click_on 'attend'
 
       within '#attendees' do
         should_see_selector "a[title='Paco Pérez']"
@@ -38,7 +38,7 @@ feature 'Attendees' do
         should_see_selector "a[title='Paco Pérez']"
       end
 
-      click_on 'Unattend'
+      click_on 'unattend'
 
       within '#attendees' do
         should_not_see_selector "a[title='Paco Pérez']"
